9d8c23cdbb78640a7625c0c898ba36fca7166cf0
MyeongJun0911 <kmyeongjun124@gmail.com>
2023-11-13 11:22:12 +0900

    Main.main 대문자 수정!

--- src/com/sh/app/Main.java
+++ src/com/sh/app/Main.java
@@ -8,8 +8,8 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello world");
-		System.out.println("Hello Git");
+		System.out.println("HELLO WORLD");
+		System.out.println("HELLO GIT");
 		
 		new Dog().bark();
 		new Cat().jump();
