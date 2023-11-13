d347de75ab039dce7b8ed83ec974d613bea086d3
MyeongJun0911 <kmyeongjun124@gmail.com>
2023-11-13 10:41:18 +0900

    Cat#jump 추가

---
+++ src/com/sh/animal/Dog.java
@@ -0,0 +1,9 @@
+package com.sh.animal;
+
+public class Dog {
+
+	public void bark() {
+		System.out.println("멍멍~");
+	}
+
+}
--- src/com/sh/app/Main.java
+++ src/com/sh/app/Main.java
@@ -1,11 +1,17 @@
 package com.sh.app;
 
+import com.sh.animal.Cat;
+import com.sh.animal.Dog;
+
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello world");
 		System.out.println("Hello Git");
+		
+		new Dog().bark();
+		new Cat().jump();
 	}
 
 }
